21779f0bbd26d30d05860023dcc1150389c3d18a
Nathan <19powelln@flintridgeprep.org>
2019-04-07 21:57:38 -0500

    Mostly finished CalcAlgs by now, starting to add some comments

--- CalcAlgs.java
+++ CalcAlgs.java
@@ -1,35 +1,55 @@
 public class CalcAlgs{
+    static NullPointerException n; //If I do this it helps (honestly not completely sure why)
     public int getMax(int[] a){
-
-        if ( a.length==0){
-            return 0;
-        }
-        int maxValue = a[0];
-        for (int i=0; i<a.length; i++){
-            if(a[i]>maxValue){
-                maxValue = a[i];
+        try{
+            if (a.length==0){
+                throw n; //Makes the NullPointerException happen, don't need to do it this way but figured I might as well be consistent
             }
-        }
-        return maxValue;
+            int maxValue = a[0];
+            for (int i=0; i<a.length; i++){
+                if(a[i]>maxValue){
+                    maxValue = a[i];
+                }
+            }
+            return maxValue;
+        } catch (NullPointerException n){
+            System.out.println("This array is a null array, but here goes nothing! ");
+            return -0; //I have to return an integer of some type, and I chose 0
+        } 
     }
 
     public int getMin(int[] a){
-        int minValue = a[0];
-        for (int i=0; i<a.length; i++){
-            if(a[i]<minValue){
-                minValue = a[i];
+        try{
+            if (a.length==0){
+                throw n; //Makes the NullPointerException happen
             }
+            int minValue = a[0];
+            for (int i=0; i<a.length; i++){
+                if(a[i]<minValue){
+                    minValue = a[i];
+                }
+            }
+            return minValue;
+        } catch (NullPointerException n){
+            System.out.println("This array is a null array, but here goes nothing! ");
+            return -0; //I have to return an integer of some type, and I chose 0
         }
-        return minValue;
     }
 
-    static public double getAvg(int[] a){
-        double sumValue=0;
-        //sumValue = a[0];
-        for (int i=0; i<a.length; i++){
-            sumValue=sumValue+a[i];
+    static public double getMean(int[] a){ //has to be a double because you say so/an average often is one
+        try{
+            if (a.length==0){
+                throw n; //Makes the NullPointerException happen
+            }
+            double sumValue=0;
+            for (int i=0; i<a.length; i++){ //This for loop sums up the entirety of the array
+                sumValue=sumValue+a[i]; 
+            }
+            return sumValue/a.length; //divides the sum of the entire array by the length of the array (aka the mean)
+        } catch (NullPointerException n){
+            System.out.println("This array is a null array, but here goes nothing! ");
+            return -0; //I have to return an integer of some type, and I chose 0
         }
-        return sumValue/a.length;
     }
 
     static public void main(){
@@ -37,9 +57,19 @@ public class CalcAlgs{
         int[] t1 = {152, 15, 3, 151, 19, 112};
         int[] t2 = {152, 1, 3011, 157, -11, 0};
         int[] t3 = {-152, -15, -3, -151, -19, 0};
+        int[] a1 = {152, 15, 3, 151, 19, 112};
+        int[] a2 = {};
+        int[] a3 = {1,1,1,1,1,1,1,1,2};
+        int[] a4 = {-152, -15, -3, -151, -19, 0};
         CalcAlgs test = new CalcAlgs();
-        System.out.println("Maximum = "+test.getMax(numbers));
+        System.out.println("Maximum = "+test.getMax(new int[] {}));
+        System.out.println("Maximum1 = "+test.getMax(t1));
+        System.out.println("Maximum2 = "+test.getMax(t2));
+        System.out.println("Maximum3 = "+test.getMax(t3));
         System.out.println("Minimum = "+test.getMin(numbers));
-        System.out.println("Minimum = "+test.getAvg(numbers));
+        System.out.println("Mean1 = "+test.getMean(a1));
+        System.out.println("Mean2 = "+test.getMean(a2));
+        System.out.println("Mean3 = "+test.getMean(a3));
+        System.out.println("Mean4 = "+test.getMean(a4));
     }
 }
